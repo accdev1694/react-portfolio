3b43c98e32e5d3ce39ac7c8403bded89f1701f8d
unknown <diloch111@gmail.com>
2025-04-09 14:17:16 +0100

    create leftIntroContent and logic as template component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,19 +1,10 @@
 import myPhoto from "../assets/images/my-photo.jpg";
-import { useNavigate } from "react-router-dom";
+import LeftIntroContent from "./LeftIntroContent";
+
 const HomePage = ({ leftHero }) => {
-  const { heading, subHeading, description, learnMore } = leftHero[0].home;
-  const navigate = useNavigate()
-  
   return (
     <div className="hero">
-      <div className="left-intro">
-        <h1 className="main-section-heading">{heading}</h1>
-        <h2 className="hero-subheading">{subHeading}</h2>
-        <p className="description">{description}</p>
-        <button onClick={()=>{navigate('/my-skills')}} className="learn-more">
-          {">>>"} {learnMore}
-        </button>
-      </div>
+      <LeftIntroContent leftHero={leftHero} page={0} nextPage={"/my-skills"} />
       <div className="right-intro">
         <img className="my-photo" src={myPhoto} alt="my passport photograph" />{" "}
       </div>
---
+++ src/components/LeftIntroContent.jsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
+const LeftIntroContent = ({ leftHero, page, nextPage }) => {
+  const { heading, subHeading, description, learnMore } = leftHero[page].home;
+  const navigate = useNavigate();
+  return (
+    <div className="left-intro">
+      <h1 className="main-section-heading">{heading}</h1>
+      <h2 className="hero-subheading">{subHeading}</h2>
+      <p className="description">{description}</p>
+      <button
+        onClick={() => {
+          navigate(nextPage);
+        }}
+        className="learn-more"
+      >
+        {learnMore}
+      </button>
+    </div>
+  );
+};
+
+export default LeftIntroContent;
