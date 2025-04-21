7355e7217c38ee3dfbcb08a6aa6247409b3e624f
unknown <diloch111@gmail.com>
2025-04-21 02:07:34 +0100

    add responsiveness to pages

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
+import { getImageUrl } from "../../../utils";
 import Navlinks from "../Navlinks/Navlinks";
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
   return (
     <>
       <Link to={"/"} className={styles.logo}>
         Portfoli<span className={styles.colorChange}>o</span>
       </Link>
       <div className="links">
-        <button className="burgerMenu">code here</button>
-        <Navlinks />
+        <img
+          className={styles.burgerMenu}
+          src={isMenuOpen ? getImageUrl("./assets/images/nav/closeIcon.png") : getImageUrl("./assets/images/nav/menuIcon.png")}
+          alt=""
+          onClick={()=>setIsMenuOpen(!isMenuOpen)}
+        />
+
+        <Navlinks isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
       </div>
     </>
   );
--- src/components/layout/Navlinks/Navlinks.jsx
+++ src/components/layout/Navlinks/Navlinks.jsx
@@ -1,9 +1,9 @@
 import styles from "./Navlinks.module.css"
 import { Link } from "react-router-dom";
 
-const Navlinks = () => {
+const Navlinks = ({isMenuOpen, setIsMenuOpen}) => {
     return ( 
-        <div className={styles.navlinks}>
+        <div className={`${styles.navlinks} ${!isMenuOpen && styles.menuClosed}`} onClick={()=>setIsMenuOpen(false)}>
             <Link to={"/"}>homePage</Link>
             <Link to={"/skills"}>mySkills</Link>
             <Link to={"/projects"}>myProjects</Link>
